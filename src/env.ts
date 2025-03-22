fb29f39954c4db65fc0ab134350670474427bb4c
WilliamGrohe <william.grohe@gmail.com>
2025-03-21 23:34:22 -0300

    Testando .nullish(), zod .env

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  CLOUDFLARE_ENDPOINT: z.string().url(),
-  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
-  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
-  CLOUDFLARE_BUCKET_NAME: z.string(),
+  DATABASE_URL: z.string().url().nullish(),
+  CLOUDFLARE_ENDPOINT: z.string().url().nullish(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string().nullish(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string().nullish(),
+  CLOUDFLARE_BUCKET_NAME: z.string().nullish(),
 })
 
 export const env = envSchema.parse(process.env)
